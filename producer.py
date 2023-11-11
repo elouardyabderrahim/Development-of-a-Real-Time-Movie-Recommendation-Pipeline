c51039682f597316f1fc3b96c7d23752d4f2f80d
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-11-11 15:18:41 +0100

    feat: create infinite loop to send to consumer

--- producer.py
+++ producer.py
@@ -1,16 +1,14 @@
 from kafka import KafkaProducer
 import json
 import requests
-from config import KEY_API_bd  # Import API key from config
+from config import KEY_API, KAFKA_BOOTSTRAP_SERVERS, KAFKA_TOPIC  
+import time
 
-API_KEY = KEY_API_bd
 BASE_URL = "https://api.themoviedb.org/3/movie/popular"
 LANGUAGE = "en-US"
-KAFKA_BOOTSTRAP_SERVERS = 'localhost:9092'
-KAFKA_TOPIC = 'movie_data_topic'
 
 def fetch_movie_data(page):
-    MOVIE_ENDPOINT = f"{BASE_URL}?api_key={API_KEY}&language={LANGUAGE}&page={page}"
+    MOVIE_ENDPOINT = f"{BASE_URL}?api_key={KEY_API}&language={LANGUAGE}&page={page}"
     response = requests.get(MOVIE_ENDPOINT)
 
     if response.status_code == 200:
@@ -19,26 +17,26 @@ def fetch_movie_data(page):
         print(f"Error fetching data from TMDb API for page {page}")
         return []
 
-# Number of pages you want to retrieve
-num_pages = 100
-all_movie_data = []
-
-for page_number in range(1, num_pages + 1):
-    movie_data = fetch_movie_data(page_number)
-    all_movie_data.extend(movie_data)
-
-# Create Kafka producer instance
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
+    key_serializer=lambda k: str(k).encode('utf-8'),
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-# Produce messages to Kafka topic
-for movie in all_movie_data:
-    # Produce the message to the Kafka topic
-    producer.send(KAFKA_TOPIC, key=str(movie['id']), value=movie)
+try:
+    page_number = 1
+    while True:
+        movie_data = fetch_movie_data(page_number)
+        print(f"Sending data for page {page_number} to Kafka")
+        
+        for movie in movie_data:
+            producer.send(KAFKA_TOPIC, key=movie['id'], value=movie)
 
-# Close the producer
-producer.close()
-
-print(f"Data for {num_pages} pages has been successfully sent to Kafka topic {KAFKA_TOPIC}.")
+        page_number += 1
+        time.sleep(2)  
+except KeyboardInterrupt:
+    print("Script interrupted. Closing the producer.")
+    producer.close()
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+    producer.close()
